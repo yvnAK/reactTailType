d83613a6d5f012f7ef865f2cad0db98d9886134b
yvnAK <57645366+yvnAK@users.noreply.github.com>
2025-08-14 11:27:25 +0930

    Removed styling elements. Recreating complete UI in a different direction and used some Tailwind to get started. Also have some hardcoded values for data

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="py-10 h-screen">
+      {/* main class now has a vertical (y) padding of 10, and takes up the height of the screen */}
+      <h1 className="font-bold text-3xl text-center">Your Todos</h1>
+    </main>
   )
 }
 
---
+++ src/data/todos.ts
@@ -0,0 +1,17 @@
+export const dummyData = [
+    {
+        id: 1, 
+        title: "Like this video",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "Smash that subscribe button",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "Leave a comment below",
+        completed: false,
+    },
+];
